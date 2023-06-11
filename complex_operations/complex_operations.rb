257c5c2a407d4c03e1828ecc9d31c70552e5e7ee
Anton Panteleev <friendlyantz@pm.me>
2023-06-11 21:01:21 +1000

    Refac Complex operration. added 5sec wait for Jaeger

--- complex_operations/complex_operations.rb
+++ complex_operations/complex_operations.rb
@@ -1,9 +1,10 @@
 require 'net/http'
-require 'pry'
 require 'opentelemetry/sdk'
+require 'opentelemetry/exporter/jaeger'
 require 'colorize'
 
-ENV['OTEL_TRACES_EXPORTER'] = 'console'
+# ENV['OTEL_TRACES_EXPORTER'] = 'console'
+ENV['OTEL_TRACES_EXPORTER'] = 'jaeger'
 
 OpenTelemetry::SDK.configure do |c|
   c.service_name = "COMPLEX SERVICE"
@@ -14,50 +15,45 @@ puts "We embark on a formidable undertaking #{request_id}".red if request_id
 
 MyAppTracer = OpenTelemetry.tracer_provider.tracer('LadidaTracer')
 
-# PREDICTION: 1000ms
-# start a OpenTelemetry span
-MyAppTracer.in_span("complex processes parent span") do |span|
-  # Demo: Calculation
-  # start a OpenTelemetry sub-span
+MyAppTracer.in_span("complex processes parent span") do |parent_span|
   MyAppTracer.in_span("child span 1 - calc") do |span|
-    (0..30_000_000).each{rand(20)}.sum # 100ms work
+    (0..1_000_000).each{rand(20)}.sum
     puts "Eureka! The calculation hath reached its culmination!".light_red
   end
-  # end a OpenTelemetry sub-span
 
-  # Demo: DataBase
-  # start a OpenTelemetry sub-span
   MyAppTracer.in_span("child span 2 - DB query") do |span|
-    # Slow SQLite example # aim for 4s query
     require 'sqlite3'
 
     db = SQLite3::Database.new "test.db"
     db.execute "CREATE TABLE IF NOT EXISTS numbers (number INTEGER)"
     db.execute "DELETE FROM numbers"
     db.execute "BEGIN TRANSACTION"  
-    (0..3_000_000).each{|i| db.execute "INSERT INTO numbers VALUES (#{i})"}
+    (0..300_000).each{|i| db.execute "INSERT INTO numbers VALUES (#{i})"}
     db.execute "COMMIT TRANSACTION"
     db.execute "SELECT COUNT(*) FROM numbers"
 
-    puts 'Rejoice, good folk! The search in the database be finished!'.light_red
-    # end a OpenTelemetry sub-span
+    puts 'Rejoice, good folk! The search in the database be finished!'.yellow
   end
 
-  # Demo: Network
-  # start a OpenTelemetry sub-span
-  MyAppTracer.in_span("child span 3 - HTTP request") do |span|
-    Net::HTTP.get('docs.zepto.money', '/') # 40ms unless you download something slow
-    sleep 3
-    puts "Behold, the web request is fulfilled, as if by sorcery!".light_red
+  MyAppTracer.in_span("child span 3 - Duck Duck Go it!") do |span|
+    Net::HTTP.get('duckduckgo.com', '/') # 38ms unless you download something slow
+    puts "Behold, the web request is fulfilled, as if by sorcery!".blue
   end
-  # end a OpenTelemetry sub-span
 
-  # Demo: Job
-  # start a OpenTelemetry sub-span
-  MyAppTracer.in_span("child span 4 - backgroun job in sidekiq") do |span|
-    puts `ruby job.rb #{request_id}`
+  MyAppTracer.in_span("child span 4 - background job in Sidekiq") do |span|
+    require File.join(__dir__,'job.rb')
+    # Slow SQLite example # aim for 4s query
+    CoolJob.perform
+  end
+
+  MyAppTracer.in_span("child span 5 - bing") do |span|
+    # Slow SQLite example # aim for 4s query
+    Net::HTTP.get('bing.com', '/') # 38ms unless you download something slow
+    puts 'dummy DB'
   end
 
   puts 'The deed is done!'.green
-  # end a OpenTelemetry span
+    # end a OpenTelemetry sub-span
 end
+
+sleep 5 # delay for Jaegerr to catch up
--- complex_operations/job.rb
+++ complex_operations/job.rb
@@ -1,6 +1,10 @@
 require 'colorize'
 
-request_id = ARGV.empty? ? nil : ARGV.join
-puts "Aye, milord, the Sidekiq deed be done! #{request_id}".magenta if request_id
+class CoolJob
+  def self.perform
+    request_id = ARGV.empty? ? nil : ARGV.join
+    puts "Aye, milord, the Sidekiq deed be done! #{request_id}".magenta if request_id
+  end
+end
 
 # add a slow job that retries a few times
